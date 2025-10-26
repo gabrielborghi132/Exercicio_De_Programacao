46da5a7bba8240f8819759ebcb9e53c30ec04f58
gabrielborghi132 <gabrielborghi132@gmail.com>
2025-10-25 22:08:34 -0300

    Adiciona implementação do algoritmo de Dijkstra em main.cpp para resolução do problema Mania de Par

---
+++ beecrowd/Grafos/1931 - Mania de Par/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using tiii = tuple<int, int, int>;
+#define endl '\n'
+const int INF = 0x3f3f3f3f;
+const int MAXN = 1e5 + 5;
+
+int n, m, s, f;                 // n = nós, m = arestas, s = origem, f = destino
+vector<pair<int, int>> g[MAXN]; // grafo (v, peso)
+int dist[MAXN][2];              // vetor de distâncias
+
+// algoritmo de dijkstra
+void dijkstra(int s)
+{
+    dist[s][0] = 0;          // distância inicial como 0
+    priority_queue<tiii> pq;
+
+    pq.push({0, s, 0});
+    while (!pq.empty())
+    {
+        auto [d, u, p] = pq.top();
+        pq.pop();
+        if (-d > dist[u][p])
+            continue; // ignora se já há caminho melhor
+        for (auto [v, w] : g[u]) {
+            int np = 1 - p; // alterna paridade
+            if (dist[u][p] + w < dist[v][np]) {
+                dist[v][np] = dist[u][p] + w;
+                pq.push({-dist[v][np], v, np});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    memset(dist, INF, sizeof(dist)); // inicializa distâncias com infinito
+    
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].emplace_back(v, w);
+        g[v].emplace_back(u, w);
+    }
+
+    dijkstra(1); // executa dijkstra a partir do nó 1
+
+    if (dist[n][0] == INF) {
+        cout << -1 << endl;
+    } else {
+        cout << dist[n][0] << endl;
+    }
+
+    return 0;
+}
