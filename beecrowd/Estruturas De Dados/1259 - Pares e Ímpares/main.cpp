b4138ea2dfdf5bc98bc5df35ecb3b47fc3a296ff
Gabriel H. N. Borghi <gabrielborghi132@gmail.com>
2025-12-06 08:38:09 -0300

    add exercicios

--- beecrowd/Estruturas De Dados/1259 - Pares e Ímpares/main.cpp
+++ beecrowd/Estruturas De Dados/1259 - Pares e Ímpares/main.cpp
@@ -1,3 +1,9 @@
+/**************************************************/
+//         1259 - Pares e Ímpares
+//https://judge.beecrowd.com/pt/problems/view/1259*/
+//
+/**************************************************/
+
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
