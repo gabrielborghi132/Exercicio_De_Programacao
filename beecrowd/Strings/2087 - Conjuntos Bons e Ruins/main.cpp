47f5fcf97975b52884c7db1b0dbb89f9d56bc7e0
Gabriel H. N. Borghi <gabrielborghi132@gmail.com>
2026-01-18 15:46:35 -0300

    add

--- beecrowd/Strings/2087 - Conjuntos Bons e Ruins/main.cpp
+++ beecrowd/Strings/2087 - Conjuntos Bons e Ruins/main.cpp
@@ -7,68 +7,66 @@ struct node
     int next[26];
     int degree;
     int terminal_count;
-    int prefix_count;
 
     node()
     {
         fill(next, next + 26, -1);
         degree = 0;
         terminal_count = 0;
-        prefix_count = 0;
     }
 };
+
 vector<node> trie(1);
 
-void add_string(const string &s)
+bool add_string(const string &s)
 {
     int v = 0;
 
-    for (auto ch : s)
+    for (char ch : s)
     {
-        int c = ch - 'a';
+        if (trie[v].terminal_count > 0)
+            return true;
 
+        int c = ch - 'a';
         if (trie[v].next[c] == -1)
         {
-
-            trie[v].next[c] = trie.size();
-
+            trie[v].next[c] = (int)trie.size();
             trie.emplace_back();
-
             trie[v].degree++;
         }
-
         v = trie[v].next[c];
-
-        trie[v].prefix_count++;
     }
 
+    if (trie[v].terminal_count > 0)
+        return true;
+    if (trie[v].degree > 0)
+        return true;
+
     trie[v].terminal_count++;
+    return false;
 }
+
 int main()
 {
     int n;
     while (cin >> n, n)
     {
-        string s;
         trie.clear();
-        trie.emplace_back(1);
+        trie.emplace_back();
+
+        bool ruim = false;
+        string s;
+
         for (int i = 0; i < n; i++)
         {
             cin >> s;
-            add_string(s);
+            if (!ruim)
+                ruim = add_string(s);
+            else
+                add_string(s);
         }
-        bool ved = true;
-        for (auto &a : trie)
-        {
-            if (a.prefix_count > 1)
-            {
-                cout << "Conjunto Ruim" << endl;
-                ved = false;
-            }
-        }
-        if (ved)
-        {
-            cout << "Conjunto Bom" << endl;
-        }
+
+        cout << (ruim ? "Conjunto Ruim" : "Conjunto Bom") << endl;
     }
+    return 0;
 }
