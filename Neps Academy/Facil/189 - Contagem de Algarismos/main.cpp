2ab55c4ded7288642bedf0c9762106cbbe295f46
Gabriel H. N. Borghi <gabrielborghi132@gmail.com>
2026-03-28 10:51:27 -0300

    add exer

--- Neps Academy/Facil/189 - Contagem de Algarismos/main.cpp
+++ Neps Academy/Facil/189 - Contagem de Algarismos/main.cpp
@@ -2,23 +2,18 @@
 using namespace std;
 #define endl '\n'
 using ll = long long;
-
-int main()
-{
-    ll n;
-    string s;
-    cin >> n;
-    vector<ll> vt(10, 0);
-    for (ll i = 0; i < n; i++)
-    {
-        cin >> s;
-        for (auto &x : s)
-        {
-            vt[x - '0']++;
-        }
+int main() {
+  ll n;
+  string s;
+  cin >> n;
+  vector<ll> vt(10, 0);
+  for (ll i = 0; i < n; i++) {
+    cin >> s;
+    for (auto &x : s) {
+      vt[x - '0']++;
     }
-    for (ll i = 0; i < 10; i++)
-    {
-        cout << i << " - " << vt[i] << endl;
-    }
+  }
+  for (ll i = 0; i < 10; i++) {
+    cout << i << " - " << vt[i] << endl;
+  }
 }
